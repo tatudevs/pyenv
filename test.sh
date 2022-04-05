babe0d05af1f75041dae4c4bf228c6a353ca3b85
George Nachman <gnachman@gmail.com>
2022-04-05 13:46:58 -0700

    Enable static build of python 3.10.4

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ export KEEP_BUILD_PATH=1
 export DEBUG=1
 export CFLAGS="-mmacosx-version-min=12.0"
 #arch -x86_64  /usr/bin/sandbox-exec -f ~/pyenv.sb bin/pyenv install 3.8.13 | tee output 2>&1
-arch -x86_64  /usr/bin/sandbox-exec -f ~/pyenv.sb bin/pyenv install 3.7.13 | tee output 2>&1
+#arch -x86_64  /usr/bin/sandbox-exec -f ~/pyenv.sb bin/pyenv install 3.7.13 | tee output 2>&1
+arch -x86_64  /usr/bin/sandbox-exec -f ~/pyenv.sb bin/pyenv install 3.10.4 | tee output 2>&1
 
